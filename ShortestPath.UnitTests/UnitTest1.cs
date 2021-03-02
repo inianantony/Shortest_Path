f314ec6ec9609216870ab28bf2f48c132bbfc947
Inian <antony.inian@zuhlke.com>
2021-03-02 21:14:32 +0800

    Add project files.

---
+++ ShortestPath.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Shortest_Path;
+
+namespace ShortestPath.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
---
+++ Shortest_Path/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shortest_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
