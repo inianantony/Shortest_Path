562e9cd9c70dc986159b19b5ce4de98aaef5f432
anin <antony.inian@zuhlke.com>
2021-03-07 17:00:49 +0800

    Add start time into the API input

--- dev.sh
+++ dev.sh
@@ -4,5 +4,8 @@ echo "************** Preparing to build docker image *****************"
 docker build -t shortest_path:latest -f Dockerfile . --progress=plain
 echo "******************* Completed Building Docker Image *******************"
 
-echo "******************* Running Image *******************"
+echo "******************* Running Image without Start Time *******************"
 docker run -it docker.io/library/shortest_path:latest --start="Holland Village" --end="Bugis" --csvpath=StationMap.csv
+
+echo "******************* Running Image With Start Time *******************"
+docker run -it docker.io/library/shortest_path:latest --start="Holland Village" --end="Bugis" --csvpath=StationMap.csv --starttime=2021-03-05T23:30
